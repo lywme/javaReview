31b8c4d9deae608f616c026106832840226956a4
Yewei Li <38413224+lywme@users.noreply.github.com>
2019-10-11 22:35:26 -0400

    code review

---
+++ bubbleSort.java
@@ -0,0 +1,48 @@
+package com.company;
+import java.util.Arrays;
+import java.util.Random;
+//Implement an Array Structure
+
+
+public class Main{
+	public static void main(String[] args)
+	{
+		//Initialize a random array to sort
+		int[] array=new int[20];
+		Random ran=new Random();
+		for(int i=0;i<array.length;i++)
+		{
+			//assign each item a value range from 0~99
+			array[i]=ran.nextInt(100);
+		}
+
+		System.out.println("Before Bubble Sort: "+Arrays.toString(array));
+		bubbleSort(array);
+		System.out.println("After Bubble Sort : "+Arrays.toString(array));
+
+	}
+
+	public static void bubbleSort(int[] arr)
+	{
+		for(int lastUnsortedPart=arr.length-1;lastUnsortedPart>0;lastUnsortedPart--)
+		{
+			for(int j=0;j<lastUnsortedPart;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					swapArray(arr,j,j+1);
+				}
+			}
+		}
+	}
+
+	public static void swapArray(int[] arr,int i,int j)
+	{
+		if(i==j)
+			return;
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+	}
+}
+
