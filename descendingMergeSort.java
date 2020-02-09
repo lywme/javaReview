f3613cbb85ef15fbf8e867113120bfb58a4af587
Yewei Li <38413224+lywme@users.noreply.github.com>
2020-02-08 23:02:29 -0500

    Merge sort in descending order

---
+++ descendingMergeSort.java
@@ -0,0 +1,52 @@
+package com.company;
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class Test{
+    public static void main(String[] args)
+    {
+        //Initialize a random array to sort
+        int[] array=new int[20];
+        Random ran=new Random();
+        for(int i=0;i<array.length;i++)
+        {
+            //assign each item a value range from 0~99
+            array[i]=ran.nextInt(100);
+        }
+
+        System.out.println("Before Merge Sort: "+Arrays.toString(array));
+        mergeSort(array,0,array.length);
+        System.out.println("After Merge Sort : "+Arrays.toString(array));
+
+    }
+
+    public static void mergeSort(int[] array,int startIndex, int endIndex)
+    {
+        if(endIndex-startIndex<2)
+            return;
+        int mid=(endIndex+startIndex)/2;
+        mergeSort(array,startIndex,mid);
+        mergeSort(array,mid,endIndex);
+        merge(array,startIndex,mid,endIndex);
+    }
+
+    public static void merge(int[] array,int startIndex,int mid,int endIndex)
+    {
+        if(array[mid-1]>=array[mid])
+            return;
+
+        int i=startIndex;
+        int j=mid;
+        int tempIndex=0;
+        int[] tempArray=new int[endIndex-startIndex];
+        while(i<mid&&j<endIndex)
+        {
+            tempArray[tempIndex++]=array[i]>=array[j]?array[i++]:array[j++];
+        }
+        //copy any elements left in the left array
+        System.arraycopy(array,i,array,startIndex+tempIndex,mid-i);
+        //copy the auxiliary array back to original array
+        System.arraycopy(tempArray,0,array,startIndex,tempIndex);
+    }
+}
