1a1bc23c335eb3be96a8d3db51801c2eaf250f48
Yewei Li <38413224+lywme@users.noreply.github.com>
2020-10-02 14:39:34 -0400

    Create fortinet.java

---
+++ fortinet.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class scratch
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(getEncryptedName("Kate Winslet"));
+    }
+
+    public static boolean validate(String name)
+    {
+        return true;
+    }
+
+
+    public static String getEncryptedName(String name)
+    {
+        StringBuilder sb=new StringBuilder();
+        if(validate(name))
+        {
+            char[] charArr=name.toCharArray();
+            for(int i=0;i<name.length();i++)
+            {
+                if(charArr[i]>='a' && charArr[i]<='z')
+                {
+                    charArr[i]=Character.toUpperCase(charArr[i]);
+                }
+                else
+                {
+                    charArr[i]=Character.toLowerCase(charArr[i]);
+                }
+            }
+
+            for(int i=name.length()-1;i>=0;i--)
+            {
+                sb.append(charArr[i]);
+            }
+
+            return sb.toString();
+        }
+        else
+        {
+            throw new IllegalArgumentException("Try again with valid name");
+        }
+    }
+
+}
+
+
